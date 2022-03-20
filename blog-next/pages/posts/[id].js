1e472c4f7dd803b3897fda6bf23ec3f29e4ebed4
abu-git <abu.sammie@gmail.com>
2022-03-20 20:19:46 +0200

    post page build success

--- blog-next/pages/posts/[id].js
+++ blog-next/pages/posts/[id].js
@@ -1,9 +1,42 @@
 import React from 'react'
+import axios from 'axios'
 
-function PostPage() {
-  return (
-    <>This is the post page</>
+
+function PostPage({post}) {
+    return (
+        <>
+            <article>
+                <header>
+                    <h1>{post.data.attributes.title}</h1>
+                    <h2>{post.data.attributes.description}</h2>
+                </header>
+            </article>
+        </>
   )
 }
 
 export default PostPage
+
+export async function getStaticProps({params}) {
+    const postResponse = await axios.get(`http://localhost:1337/api/posts/${params.id}`)
+
+    //console.log(postResponse)
+    return {
+        props: {
+            post: postResponse.data
+        }
+    }
+}
+
+export async function getStaticPaths() {
+    const postsResponse = await axios.get("http://localhost:1337/api/posts")
+
+    //console.log(postsResponse.data.data)
+    const paths = postsResponse.data.data.map((post) => {
+        return {params: {id: post.id.toString()}}
+    })
+    return {
+        paths,
+        fallback: false,
+    }
+}
