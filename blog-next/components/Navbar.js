4d9af80a8b31ac6472d37b71395449a81312c310
abu-git <abu.sammie@gmail.com>
2022-03-20 13:50:29 +0200

    some scss styling done

--- blog-next/components/Navbar.js
+++ blog-next/components/Navbar.js
@@ -4,17 +4,19 @@ import Link from 'next/link'
 function Navbar() {
   return (
     <nav className='navbar'>
-        <Link href="/">
-            <a>Devistry</a>
-        </Link>
+        <div className='container'>
+            <Link href="/">
+                <a className='title'>Devistry</a>
+            </Link>
 
-        <ul>
-            <li>
-                <Link href="/posts">
-                    <a>All posts</a>
-                </Link>
-            </li>
-        </ul>
+            <ul>
+                <li>
+                    <Link href="/posts">
+                        <a>All posts</a>
+                    </Link>
+                </li>
+            </ul>
+        </div>
     </nav>
   )
 }
